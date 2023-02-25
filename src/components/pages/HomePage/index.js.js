5e9aefd16575c6b442ef1ba7d258fa50ad442c03
maxnatalia <Nataliamazur1988@gmail.com>
2023-02-25 11:50:30 +0100

    added HomePage

---
+++ src/components/pages/HomePage/index.js.js
@@ -0,0 +1,16 @@
+import Characters from "./Characters";
+import Form from "./Form";
+
+
+const HomePage = () => {
+
+    return (
+        <>
+            <h1>HomePage - Rick and Morty App</h1>
+            <Form />
+            <Characters />
+        </>
+    )
+};
+
+export default HomePage;
