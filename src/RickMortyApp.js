54326cd1865ae94be58601891e09751023103852
maxnatalia <Nataliamazur1988@gmail.com>
2023-02-26 11:17:38 +0100

    added new path - Locations

--- src/RickMortyApp.js
+++ src/RickMortyApp.js
@@ -2,12 +2,14 @@ import { Routes, Route, HashRouter } from "react-router-dom";
 import HomePage from "./components/pages/HomePage/index.js";
 import ErrorPage from "./components/pages/ErrorPage";
 import SingleCharacter from "./components/pages/SingleCharacter";
+import Locations from "./components/pages/HomePage/Locations.js";
 
 const RickMortyApp = () => {
     return (
         <HashRouter>
             <Routes>
-                <Route path="/character/:id" element={<SingleCharacter />} />
+                <Route path="/character/:paramId" element={<SingleCharacter />} />
+                <Route path="/locations" element={<Locations />} />
                 <Route path="/" element={<HomePage />} />
                 <Route path="*" element={<ErrorPage />} />
             </Routes>
