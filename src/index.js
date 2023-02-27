49b333655f1dcf19a7fc4da725cf5a69c42906c1
maxnatalia <Nataliamazur1988@gmail.com>
2023-02-27 14:35:18 +0100

    updated main file - RickMortyApp in index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from "react-query/devtools";
+import RickMortyApp from './RickMortyApp';
+
 
 const queryClient = new QueryClient();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient} >
       <ReactQueryDevtools />
-      <App />
+      <RickMortyApp />
     </QueryClientProvider>
   </React.StrictMode>
 );
