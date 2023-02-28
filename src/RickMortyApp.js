e5f74fc2dec7666687fb1c524710d2b25bf3500e
maxnatalia <Nataliamazur1988@gmail.com>
2023-02-28 11:03:58 +0100

    added new route - EpisodesPage

--- src/RickMortyApp.js
+++ src/RickMortyApp.js
@@ -1,15 +1,19 @@
 import { Routes, Route, HashRouter } from "react-router-dom";
-import HomePage from "./components/pages/HomePage/index.js";
+import HomePage from "./components/pages/HomePage.js";
 import ErrorPage from "./components/pages/ErrorPage";
-import SingleCharacter from "./components/pages/SingleCharacter";
-import Locations from "./components/pages/HomePage/Locations.js";
+import SingleCharacterPage from "./components/pages/SingleCharacterPage";
+import LocationsPage from "./components/pages/LocationsPage.js";
+import CharactersPage from "./components/pages/CharactersPage";
+import EpisodesPage from "./components/pages/EpisodesPage";
 
 const RickMortyApp = () => {
     return (
         <HashRouter>
             <Routes>
-                <Route path="/character/:paramId" element={<SingleCharacter />} />
-                <Route path="/locations" element={<Locations />} />
+                <Route path="/character/:paramId" element={<SingleCharacterPage />} />
+                <Route path="/characters" element={<CharactersPage />} />
+                <Route path="/locations" element={<LocationsPage />} />
+                <Route path="/episode" element={<EpisodesPage />} />
                 <Route path="/" element={<HomePage />} />
                 <Route path="*" element={<ErrorPage />} />
             </Routes>
