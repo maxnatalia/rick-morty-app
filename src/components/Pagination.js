c80305e5e5c29d916e0b52b0a864d1cadbc9ae6c
maxnatalia <Nataliamazur1988@gmail.com>
2023-03-03 09:39:10 +0100

    updated Pagination component

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,15 +1,15 @@
 import { usePagerContext } from "../context/pager-context";
 
 const Pagination = ({ text, infoPage, disabled }) => {
-    const pagerData = usePagerContext();
+    const { page, prevPage, nextPage } = usePagerContext();
 
     return (
         <div>
             <p>Paginacja</p>
             <p>{text}</p>
-            <button onClick={pagerData.prevPage} disabled={pagerData.page === 1}>Prev</button>
+            <button onClick={prevPage} disabled={page === 1}>Prev</button>
             <span>{infoPage}</span>
-            <button onClick={pagerData.nextPage} disabled={disabled}>Next</button>
+            <button onClick={nextPage} disabled={disabled}>Next</button>
         </div>
     )
 };
