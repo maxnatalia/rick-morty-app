04791156c24dd8e6006a38f656d7d44527079d5e
maxnatalia <Nataliamazur1988@gmail.com>
2023-03-04 09:39:06 +0100

    updated ErrorPage - added Link to Home

--- src/components/pages/ErrorPage.js
+++ src/components/pages/ErrorPage.js
@@ -1,9 +1,13 @@
-
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
     return (
-        <div>ErrorPage</div>
+        <div>
+            <h1>Error...</h1>
+            <p>Sorry, an error has occured...</p>
+            <Link to="/">Back Home</Link>
+        </div>
     )
-}
+};
 
 export default ErrorPage;
