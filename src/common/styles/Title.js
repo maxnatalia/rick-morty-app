02e8a5d9de30e94aa1158fbd52d6153521c16a6f
maxnatalia <Nataliamazur1988@gmail.com>
2023-03-08 07:19:12 +0100

    created common styles for Title

---
+++ src/common/styles/Title.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+    margin: 0;
+    text-align: center;
+    padding: 20px;
+    font-size: 56px;
+    letter-spacing: 4px;
+    text-shadow: 4px 4px 6px rgba(66, 68, 90, 1);
+`;
