aaae95e49dd8728baeef673bac65a8e2633b6d77
maxnatalia <Nataliamazur1988@gmail.com>
2023-03-12 15:19:39 +0100

    improved common style for Title - media queries

--- src/common/styles/Title.js
+++ src/common/styles/Title.js
@@ -1,10 +1,16 @@
 import styled from "styled-components";
 
-export const Title = styled.h1`
+export const Title = styled.h2`
+    font-family: 'Creepster', cursive;
     margin: 0;
     text-align: center;
     padding: 20px;
-    font-size: 56px;
+    font-size: 70px;
     letter-spacing: 4px;
     text-shadow: 4px 4px 6px rgba(66, 68, 90, 1);
+    color: rgb(217,209,198);
+
+    @media (max-width: 767px) {
+        font-size: 42px;
+    }
 `;
