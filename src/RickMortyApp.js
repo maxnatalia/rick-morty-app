4fc8efe3e17536731a0f0ee6e786619821aaae75
maxnatalia <Nataliamazur1988@gmail.com>
2023-03-12 15:15:55 +0100

    improved main path for pages

--- src/RickMortyApp.js
+++ src/RickMortyApp.js
@@ -1,10 +1,10 @@
 import { Routes, Route, HashRouter } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
-import ErrorPage from "./components/pages/ErrorPage";
-import SingleCharacterPage from "./components/pages/SingleCharacterPage";
+import ErrorPage from "./pages/error/ErrorPage";
+import SingleCharacterPage from "./pages/singleCharacter/SingleCharacterPage";
 import LocationsPage from "./pages/locations/LocationsPage";
 import CharactersPage from "./pages/characters/CharactersPage";
-import EpisodesPage from "./components/pages/EpisodesPage";
+import EpisodesPage from "./pages/episodes/EpisodesPage";
 
 const RickMortyApp = () => {
     return (
