ebdcd3ec316092b9c72cb0c4c8c4ac91ca781a38
maxnatalia <Nataliamazur1988@gmail.com>
2023-03-13 11:37:15 +0100

    improved background for Container style

--- src/common/styles/Container.js
+++ src/common/styles/Container.js
@@ -3,9 +3,9 @@ import background from "./background.jpg";
 
 export const Container = styled.div`
     background-blend-mode: screen;
-    background-image: linear-gradient(to right bottom, black, white), url("${background}");
-    background-repeat: no-repeat;
+    background-image: linear-gradient(to right, rgb(217,209,198), rgb(217,209,198, .4)), url("${background}");
+    background-repeat: repeat-y;
     background-position: center;
-    background-size: cover;
+    background-size: 100%;
     min-height: 100vh;
 `;
