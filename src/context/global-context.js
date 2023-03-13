6091089ff053e3a22866c1fce5bd09a0f9077bf6
maxnatalia <Nataliamazur1988@gmail.com>
2023-03-13 11:36:40 +0100

    added useSearchEpisode to global-context

--- src/context/global-context.js
+++ src/context/global-context.js
@@ -1,6 +1,7 @@
 import { useContext, createContext } from 'react'
 import { usePager } from '../hooks/usePager';
 import { useSearchCharacter } from '../hooks/useSearchCharacter';
+import useSearchEpisode from '../hooks/useSearchEpisode';
 import useSearchLocation from '../hooks/useSearchLocation';
 
 const GlobalContext = createContext();
@@ -11,8 +12,29 @@ export const GlobalProvider = ({ children }) => {
     const { page, setPage, nextPage, prevPage } = usePager();
     const { filters, setFilters, handleFilterChange, handleClear } = useSearchCharacter();
     const { name, type, dimension, handleClearLocations, handleDimension, handleType, handleName } = useSearchLocation();
+    const { nameEpisode, setNameEpisode, handleClearName, handleInputName } = useSearchEpisode();
     return (
-        <GlobalContext.Provider value={{ page, setPage, nextPage, prevPage, filters, setFilters, handleFilterChange, handleClear, name, type, dimension, handleClearLocations, handleDimension, handleType, handleName }}>
+        <GlobalContext.Provider value={{
+            page,
+            setPage,
+            nextPage,
+            prevPage,
+            filters,
+            setFilters,
+            handleFilterChange,
+            handleClear,
+            name,
+            type,
+            dimension,
+            handleClearLocations,
+            handleDimension,
+            handleType,
+            handleName,
+            handleInputName,
+            handleClearName,
+            nameEpisode,
+            setNameEpisode
+        }}>
             {children}
         </GlobalContext.Provider>
     )
