8f67f6d10fd9faf65cf5f478ea1002a2c348c7e0
maxnatalia <Nataliamazur1988@gmail.com>
2023-03-14 07:56:51 +0100

    added styles for Footer and Header

--- src/pages/home/styled.js
+++ src/pages/home/styled.js
@@ -1,49 +1,39 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
-import background from "./background.png";
 
-export const Container = styled.div`
-    background-blend-mode: screen;
-    background-image: linear-gradient(to right bottom, black, white), url("${background}");
-    background-repeat: no-repeat;
-    background-position: center;
-    background-size: cover;
-    min-height: 100vh;
-    text-align: center;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    align-items: center;
-`
+export const Header = styled.header`
+    margin: 0 auto;
+    width: 60%;
+    padding: 40px;
 
+    @media (max-width: 767px) {
+        padding: 10px;
+        width: 100%;
+    }
+`;
 export const Title = styled.h1`
-    font-size: 56px;
+    font-family: 'Creepster', cursive;
+    color: rgb(217,209,198);
+    margin: 0;
+    text-align: center;
+    padding: 20px;
+    font-size: 78px;
     letter-spacing: 4px;
     text-shadow: 4px 4px 6px rgba(66, 68, 90, 1);
+
+    @media (max-width: 767px) {
+        font-size: 58px;
+    }
 `;
 
 export const WrapperLink = styled.div`
     display: flex;
     flex-direction: column;
+    justify-content: center;
+    align-items: center;
     gap: 40px;
     padding: 20px;
 `;
 
-export const ButtonLink = styled(Link)`
-    text-decoration: none;
-    text-transform: uppercase;
-    padding: 25px 30px;
-    border: 4px solid black;
-    border-radius: 40px;
-    font-size: 24px;
-    letter-spacing: 2.5px;
-    font-weight: 900;
-    color: black;
-    cursor: pointer;
-    transition: all .3s ease;
-
-    &:hover {
-        transform: translateX(-20px) scale(1.1);
-        box-shadow: 8px 8px 24px 0px rgba(66, 68, 90, 1);
-    }
+export const Footer = styled.footer`
+    padding: 20px;
 `;
