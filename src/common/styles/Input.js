6570532595fb7b4e1448fa229244a86d15ec9b82
maxnatalia <Nataliamazur1988@gmail.com>
2023-03-14 07:55:52 +0100

    changed padding and width in Input common style

--- src/common/styles/Input.js
+++ src/common/styles/Input.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 export const Input = styled.input`
-    padding: 10px 15px;
+    min-width: 300px;
+    padding: 15px;
     border-radius: 40px;
     box-shadow: 8px 8px 24px 0px rgba(66, 68, 90, 1);
     border: 1px solid black;
