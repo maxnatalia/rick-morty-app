8c510c3b8b5cd74cca5bfb6d68d909b41d829e04
maxnatalia <Nataliamazur1988@gmail.com>
2023-03-14 07:56:31 +0100

    added Footer and Heder for HomePage

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,16 +1,23 @@
-
-import { Container, Title, WrapperLink, ButtonLink } from "./styled";
+import { Header, Title, WrapperLink, Footer } from "./styled";
+import { ButtonLink } from "../../common/styles/ButtonLink";
+import { Paragraph } from "../../common/styles/Paragraph";
+import { Container } from "../../common/styles/Container";
 
 const HomePage = () => {
-
     return (
         <Container>
-            <Title>Rick and Morty App</Title>
+            <Header>
+                <Title>Rick and Morty App</Title>
+                <Paragraph>Explore different dimensions, planets and discover characters with Rick and Morty App</Paragraph>
+            </Header>
             <WrapperLink>
                 <ButtonLink to="/characters">Characters</ButtonLink>
                 <ButtonLink to="/locations">Locations</ButtonLink>
                 <ButtonLink to="/episode">Episodes</ButtonLink>
             </WrapperLink>
+            <Footer>
+                <Paragraph>Rick and Morty App &copy; {new Date().getFullYear()} All Rights Reserved</Paragraph>
+            </Footer>
         </Container>
     )
 };
