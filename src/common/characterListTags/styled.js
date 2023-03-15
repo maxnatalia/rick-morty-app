805e03534406374130e1b7536a40d76c6065e758
maxnatalia <Nataliamazur1988@gmail.com>
2023-03-15 10:29:11 +0100

    added media queries to characterTags in styles

--- src/common/characterListTags/styled.js
+++ src/common/characterListTags/styled.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export const Tag = styled(Link)`
     text-decoration: none;
-    padding: 5px 10px;
+    padding: 10px 15px;
     border: 1px solid rgb(217,209,198);
     border-radius: 40px;
     background-color: rgba(66, 68, 90, .9);
@@ -14,4 +14,10 @@ export const Tag = styled(Link)`
         filter: brightness(120%);
         box-shadow: 8px 8px 24px 0px rgba(66, 68, 90, 1);
     }
+
+    @media (max-width: 767px) {
+        font-size: 14px;
+        padding: 5px 10px;
+    }
 `;
+
