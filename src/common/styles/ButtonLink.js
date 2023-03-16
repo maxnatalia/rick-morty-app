0aba889d7eb55dbc9bd1647c0b073fb27bea26c7
maxnatalia <Nataliamazur1988@gmail.com>
2023-03-16 11:03:57 +0100

    changed margins for ButtonLink common style

--- src/common/styles/ButtonLink.js
+++ src/common/styles/ButtonLink.js
@@ -6,8 +6,7 @@ export const ButtonLink = styled(Link)`
     text-decoration: none;
     text-transform: uppercase;
     padding: 15px 20px;
-    margin-left: 30px;
-    margin-bottom: 30px;
+    margin: 20px;
     border: 2px solid rgb(217,209,198);
     border-radius: 40px;
     font-size: 24px;
@@ -25,7 +24,7 @@ export const ButtonLink = styled(Link)`
     }
 
     @media (max-width: 767px) {
-        margin: 20px;
+        margin: 10px;
         font-size: 18px;
     }
 `;
