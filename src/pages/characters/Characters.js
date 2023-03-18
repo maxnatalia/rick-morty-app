9b5dd9210b38e69f348d2adad306f94c706ba7ab
maxnatalia <Nataliamazur1988@gmail.com>
2023-03-18 08:07:05 +0100

    added new element - icon favourites to characters

--- src/pages/characters/Characters.js
+++ src/pages/characters/Characters.js
@@ -1,26 +1,40 @@
-import { CharacterContainer, CharacterWrapper, CharacterName, Image, CharacterData, CharacterType } from "./styled";
+import { CharacterContainer, CharacterWrapper, CharacterLink, CharacterName, Image, CharacterData, CharacterType } from "./styled";
+import { useGlobalContext } from "../../context/global-context";
+import { UnFavouriteIcon, FavouriteIcon } from "../../common/styles/StarIcons";
 
 const Characters = ({ characters }) => {
+    const { isFavourite, handleFavouriteClick } = useGlobalContext();
 
     return (
         <CharacterContainer>
-            {characters?.results.map(character => {
+            {characters?.results.map((character) => {
                 return (
-                    <CharacterWrapper to={`/character/${character.id}`} key={character.id}>
-                        <CharacterName>{character.name}</CharacterName>
-                        <Image src={character.image} alt={character.name} />
-                        <CharacterData>
-                            <span>Gender: </span>
-                            <CharacterType>{character.gender}</CharacterType>
-                        </CharacterData>
-                        <CharacterData>
-                            <span>Species: </span>
-                            <CharacterType>{character.species}</CharacterType>
-                        </CharacterData>
-                        <CharacterData>
-                            <span>Status: </span>
-                            <CharacterType>{character.status}</CharacterType>
-                        </CharacterData>
+                    <CharacterWrapper key={character.id}>
+                        <CharacterLink to={`/character/${character.id}`} >
+                            <CharacterName>{character.name}</CharacterName>
+                            <Image src={character.image} alt={character.name} />
+                            <CharacterData>
+                                <span>Gender: </span>
+                                <CharacterType>{character.gender}</CharacterType>
+                            </CharacterData>
+                            <CharacterData>
+                                <span>Species: </span>
+                                <CharacterType>{character.species}</CharacterType>
+                            </CharacterData>
+                            <CharacterData>
+                                <span>Status: </span>
+                                <CharacterType>{character.status}</CharacterType>
+                            </CharacterData>
+                        </CharacterLink>
+                        {isFavourite(character.id)
+                            ? <FavouriteIcon
+                                title="Remove from Favourites"
+                                onClick={() => handleFavouriteClick(character)} />
+                            : < UnFavouriteIcon
+                                pulse="true"
+                                title="Add to Favourites"
+                                onClick={() => handleFavouriteClick(character)} />
+                        }
                     </CharacterWrapper>
                 )
             })}
