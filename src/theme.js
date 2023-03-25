1f716eb919ea92193385febd6c825f354d911697
maxnatalia <Nataliamazur1988@gmail.com>
2023-03-25 07:29:55 +0100

    added theme file for styles

---
+++ src/theme.js
@@ -0,0 +1,25 @@
+export const theme = {
+    color: {
+        primaryColor: "rgb(217,209,198)",
+        lightColor: "rgb(217,209,198, .7)",
+        secondaryColor: "rgba(66, 68, 90)",
+        mediumColor: "rgba(66, 68, 90, .5)",
+        darkColor: "rgba(66, 68, 90, .9)",
+        specialColor: "rgb(251,249,118)",
+    },
+
+    borderRadius: {
+        small: "10px",
+        medium: "40px",
+        big: "50%",
+    },
+
+    boxShadow: {
+        lightShadow: `4px 4px 12px 0px rgba(66, 68, 90, .9)`,
+        darkShadow: `8px 8px 24px 0px rgba(66, 68, 90, .9)`,
+    },
+
+    breakpoint: {
+        mobile: "767px",
+    },
+};
